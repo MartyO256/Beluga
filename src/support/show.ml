46e2da9beba91f0829296edd260e4338a5a9c548
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2021-10-23 22:39:31 -0400

    Add `Show` module like in Haskell or `fp-ts`

---
+++ src/support/show.ml
@@ -0,0 +1,13 @@
+module type SHOW = sig
+  type t
+  val pp : Format.formatter -> t -> unit
+  val show : t -> string
+end
+
+module Make (T : sig
+  type t
+  val pp : Format.formatter -> t -> unit
+end) : SHOW with type t := T.t = struct
+  include T
+  let show t = Format.asprintf "%a" pp t
+end
