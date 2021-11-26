9bb5332405e6d76ff04bde97912f67f72855abd2
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2021-11-25 20:06:30 -0500

    Add `Monad` module

---
+++ src/support/monad.ml
@@ -0,0 +1,26 @@
+module type MONAD = sig
+  type 'a t
+
+  val return : 'a -> 'a t
+
+  val pure : 'a -> 'a t
+
+  val bind : ('a -> 'b t) -> 'a t -> 'b t
+
+  val ( >>= ) : 'a t -> ('a -> 'b t) -> 'b t
+end
+
+module Make (Monad : sig
+  type 'a t
+
+  val return : 'a -> 'a t
+
+  val bind : ('a -> 'b t) -> 'a t -> 'b t
+end) =
+struct
+  include Monad
+
+  let pure = return
+
+  let ( >>= ) a f = bind f a
+end
---
+++ src/support/monad.mli
@@ -0,0 +1,27 @@
+(** The abstract datatype of actions. *)
+module type MONAD = sig
+  (** The type of actions in the monad. *)
+  type 'a t
+
+  (** [return a] injects [a] into the monadic type. *)
+  val return : 'a -> 'a t
+
+  (** Alias of {!return}. *)
+  val pure : 'a -> 'a t
+
+  (** Sequentially composes two actions, passing any value produced by the
+      second action as argument to the first. *)
+  val bind : ('a -> 'b t) -> 'a t -> 'b t
+
+  (** Operator alias of {!bind}. *)
+  val ( >>= ) : 'a t -> ('a -> 'b t) -> 'b t
+end
+
+(** Functor building the aliases for a minimal implementation for {!MONAD}. *)
+module Make (Monad : sig
+  type 'a t
+
+  val return : 'a -> 'a t
+
+  val bind : ('a -> 'b t) -> 'a t -> 'b t
+end) : MONAD with type 'a t = 'a Monad.t
