9b9137b398233709e73e305f38d34a309f359069
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2021-11-27 20:50:33 -0500

    Add `Apply` module

---
+++ src/support/apply.ml
@@ -0,0 +1,62 @@
+module type APPLY = sig
+  type 'a t
+
+  val ap : 'a t -> ('a -> 'b) t -> 'b t
+
+  val ap_first : 'b t -> 'a t -> 'a t
+
+  val ( <& ) : 'a t -> 'b t -> 'a t
+
+  val ap_second : 'b t -> 'a t -> 'b t
+
+  val ( &> ) : 'a t -> 'b t -> 'b t
+
+  val seq2 : 'a1 t -> 'a2 t -> ('a1 * 'a2) t
+
+  val seq3 : 'a1 t -> 'a2 t -> 'a3 t -> ('a1 * 'a2 * 'a3) t
+
+  val seq4 : 'a1 t -> 'a2 t -> 'a3 t -> 'a4 t -> ('a1 * 'a2 * 'a3 * 'a4) t
+
+  val seq5 :
+       'a1 t
+    -> 'a2 t
+    -> 'a3 t
+    -> 'a4 t
+    -> 'a5 t
+    -> ('a1 * 'a2 * 'a3 * 'a4 * 'a5) t
+end
+
+module Make (M : Monad.MONAD) : APPLY with type 'a t = 'a M.t = struct
+  include M
+
+  let ap a f = a >>= fun a -> f >>= fun f -> return (f a)
+
+  let ap_first m2 m1 = m1 >>= fun x -> m2 >>= Fun.const (return x)
+
+  let ( <& ) m1 m2 = ap_first m2 m1
+
+  let ap_second m2 m1 = m1 >>= fun _ -> m2 >>= return
+
+  let ( &> ) m1 m2 = ap_second m2 m1
+
+  let seq2 m1 m2 = m1 >>= fun x1 -> m2 >>= fun x2 -> return (x1, x2)
+
+  let seq3 m1 m2 m3 =
+    m1 >>= fun x1 -> m2 >>= fun x2 -> m3 >>= fun x3 -> return (x1, x2, x3)
+
+
+  let seq4 m1 m2 m3 m4 =
+    m1
+    >>= fun x1 ->
+    m2 >>= fun x2 -> m3 >>= fun x3 -> m4 >>= fun x4 -> return (x1, x2, x3, x4)
+
+
+  let seq5 m1 m2 m3 m4 m5 =
+    m1
+    >>= fun x1 ->
+    m2
+    >>= fun x2 ->
+    m3
+    >>= fun x3 ->
+    m4 >>= fun x4 -> m5 >>= fun x5 -> return (x1, x2, x3, x4, x5)
+end
