fc72565c37ab46dc1c401d6d19a030db973ff78d
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-01-07 17:43:58 -0500

    Add interface file for `Token` module

---
+++ src/core/token.mli
@@ -0,0 +1,96 @@
+(** Tokens *)
+type t =
+  | EOI (** End of Input, usually the same thing as EOF. *)
+  | DUMMY
+
+  (* Symbols *)
+  | LPAREN (** ( *)
+  | RPAREN (** ) *)
+  | LBRACK (** [\[] *)
+  | RBRACK (** [\]] *)
+  | LBRACE (** [{] *)
+  | RBRACE (** [}] *)
+  | LANGLE (** < *)
+  | RANGLE (** > *)
+  | COMMA (** , *)
+  | DOUBLE_COLON (** :: *)
+  | COLON (** : *)
+  | SEMICOLON (** ; *)
+  | PIPE (** | *)
+  | TURNSTILE (** |- *)
+  | DOTS (** .. *)
+  | ARROW (** -> *)
+  | THICK_ARROW (** => *)
+  | HAT (** ^ *)
+  | DOT (** . *)
+  | LAMBDA (** \ *)
+  | STAR (** * *)
+  | EQUALS (** = *)
+  | SLASH (** / *)
+  | UNDERSCORE (** _ *)
+  | HASH (** # *)
+  | DOLLAR (** $ *)
+  | PLUS (** [+] *)
+
+  (* Keywords *)
+  | KW_AND
+  | KW_BLOCK
+  | KW_CASE
+  | KW_IF
+  | KW_THEN
+  | KW_ELSE
+  | KW_IMPOSSIBLE
+  | KW_LET
+  | KW_IN
+  | KW_OF
+  | KW_REC
+  | KW_SCHEMA
+  | KW_SOME
+  | KW_FN
+  | KW_MLAM
+  | KW_MODULE
+  | KW_STRUCT
+  | KW_END
+  | KW_TOTAL
+  | KW_TRUST
+  | KW_TYPE
+  | KW_CTYPE
+  | KW_PROP
+  | KW_INDUCTIVE
+  | KW_COINDUCTIVE
+  | KW_STRATIFIED
+  | KW_LF
+  | KW_FUN
+  | KW_TYPEDEF
+  | KW_PROOF
+  | KW_BY
+  | KW_AS
+  | KW_SUFFICES
+  | KW_TOSHOW
+
+  | STRING of string (** A string literal *)
+  | IDENT  of string (** Can mean identifier, operator, etc. *)
+  | PRAGMA of string (** Two dashes followed by an identifier *)
+  (* A dot followed by an integer; used for projections *)
+  | DOT_NUMBER of int (** .n *)
+  (* A hash followed by an identifier; used for parameter variables. *)
+  | HASH_IDENT of string (** #x *)
+  (* A dollar followed by an identifier; used for substitution variables. *)
+  | DOLLAR_IDENT of string (** $x *)
+  (* A hash followed by an underscore. *)
+  | HASH_BLANK (** #_ *)
+  (* A dollar followed by an underscore. *)
+  | DOLLAR_BLANK (** $_ *)
+  | HOLE of string (** A question mark followed by an identifier *)
+  | INTLIT  of int (** An integer literal. *)
+  | BLOCK_COMMENT of string (** A block comment of the form [%{{ ... }}%] *)
+
+val equals : t -> t -> bool
+
+type class_or_string = [ `CLASS | `TOKEN ]
+
+val print : class_or_string -> Format.formatter -> t -> unit
+
+val to_string : t -> string
+
+val class_to_string : t -> string
