c27ec79e3769ddffe532d9ca856d70373e12b606
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-03-30 14:30:03 -0400

    Make `Name.names_seq` finite.
    This simplifies the documentation.

--- src/core/signature.ml
+++ src/core/signature.ml
@@ -25,29 +25,31 @@ module Name = struct
   type fresh_name_supplier = Set.t -> t
 
   (** [find_distinct names used_names] is the first name in [names] that is
-      not a member of [used_names]. The sequence [names] is assumed to be
-      infinite, and that the elements therein are all distinct. *)
+      not a member of [used_names]. The elements in [names] are assumed to be
+      all distinct.
+
+      @raise Invalid_argument
+        if the sequence [names] is fully exhausted without being able to
+        generate a fresh name. *)
   let rec find_distinct : t Seq.t -> Set.t -> t =
    fun generate_name used_names ->
     match generate_name () with
-    | Seq.Nil -> raise @@ Invalid_argument "Finite name sequence"
+    | Seq.Nil ->
+      raise @@ Invalid_argument "Exhausted sequence of fresh names"
     | Seq.Cons (hd, tl) ->
       if Set.mem hd used_names then find_distinct tl used_names else hd
 
-  (** [names_seq base index] is the infinite sequence of names with prefix
-      [base] and incremental integer suffix starting with [index].
+  (** [names_seq base index] is the sequence of names with prefix [base] and
+      incremental integer suffix starting with [index].
 
       For instance, [names_seq "x" 1] is the sequence of names
-      [\["x1"; "x2"; ...\]].
-
-      The output sequence raises when the maximum index is reached so that
-      [Seq.Nil] is not reachable. *)
+      [\["x1"; "x2"; ...; "xM"\]] where ["M"] is [Int.max_int]. *)
   let rec names_seq : string -> int -> t Seq.t =
    fun base i ->
-    if Int.equal i Int.max_int then
-      raise @@ Invalid_argument "Maximum int reached"
-    else
-      Seq.cons (base ^ string_of_int i) (fun () -> names_seq base (i + 1) ())
+    Seq.cons
+      (base ^ string_of_int i)
+      (if Int.equal i Int.max_int then Seq.empty
+      else fun () -> names_seq base (i + 1) ())
 
   let prefixed_fresh_name_supplier base =
     find_distinct @@ Seq.cons base (names_seq base 1)
