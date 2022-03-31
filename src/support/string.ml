f7601d40fba4603c25c42856305ebc58e0ea6a4e
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-03-30 23:50:12 -0400

    Make `String` implement `ORD`

--- src/support/string.ml
+++ src/support/string.ml
@@ -3,13 +3,15 @@ include Stdlib.String
 let unpack s =
   let n = length s in
   let rec unpack i return =
-    if i < n
-    then (
+    if i < n then
       let c = get s i in
-      unpack (i + 1) (fun cs -> return (c :: cs)))
+      unpack (i + 1) (fun cs -> return (c :: cs))
     else return []
   in
   unpack 0 Fun.id
 
 let pack cs = concat "" (List.map (make 1) cs)
+
 let drop n s = sub s n (length s - n)
+
+include (Ord.Make (Stdlib.String) : Ord.ORD with type t := t)
--- src/support/string.mli
+++ src/support/string.mli
@@ -9,3 +9,7 @@ val pack : char list -> string
 (** [drop n s] is the substring of [s] without the first [n] leading
     characters. *)
 val drop : int -> string -> string
+
+(** {1 Instances} *)
+
+include Ord.ORD with type t := t
