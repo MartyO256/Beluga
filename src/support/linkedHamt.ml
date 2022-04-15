0d0b88798aad6e98224bca0b4df2943026873735
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-04-15 01:59:29 -0400

    Implement non-empty linked HAMT functor

--- src/support/linkedHamt.ml
+++ src/support/linkedHamt.ml
@@ -37,3 +37,51 @@ module Make (Hamt : HamtMisc.S) : S with type key = Hamt.key = struct
 
   let find_opt key { map; _ } = Hamt.ExceptionLess.find key map
 end
+
+module type S1 = LinkedMap.S1
+
+module Make1 (Hamt : HamtMisc.S) : S1 with type key = Hamt.key = struct
+  type key = Hamt.key
+
+  type 'a t =
+    { list : (key * 'a) Nonempty.t
+    ; map : 'a Hamt.t
+    }
+
+  let mem key { map; _ } = Hamt.mem key map
+
+  let add key value { list; map } =
+    { list = Nonempty.cons (key, value) list; map = Hamt.add key value map }
+
+  let singleton key value =
+    { list = Nonempty.singleton (key, value)
+    ; map = Hamt.singleton key value
+    }
+
+  let iter f { list; _ } =
+    Nonempty.iter (fun (key, value) -> f key value) list
+
+  let iter_bindings f { map; _ } = Hamt.iter f map
+
+  let fold_left sing cons =
+    Nonempty.fold_left
+      (fun (key, value) -> sing key value)
+      (fun acc (key, value) -> cons acc key value)
+    |> fun go { list; _ } -> go list
+
+  let fold_right sing cons =
+    Nonempty.fold_right
+      (fun (key, value) -> sing key value)
+      (fun (key, value) acc -> cons key value acc)
+    |> fun go { list; _ } -> go list
+
+  let fold_bindings sing cons { map; list } =
+    let key, value = Nonempty.head list in
+    Hamt.fold cons (Hamt.remove key map) (sing key value)
+
+  let exists p { map; _ } = Hamt.exists p map
+
+  let for_all p { map; _ } = Hamt.for_all p map
+
+  let find_opt key { map; _ } = Hamt.ExceptionLess.find key map
+end
--- src/support/linkedHamt.mli
+++ src/support/linkedHamt.mli
@@ -7,3 +7,14 @@ module type S = LinkedMap.S
 (** Functor building an implementation of the linked HAMT structure given a
     HAMT structure over ordered types. *)
 module Make (Hamt : HamtMisc.S) : S with type key = Hamt.key
+
+(** Non-empty linked association hash array mapped tries (HAMT) over ordered
+    types.
+
+    These are finite maps that keep track the sequence of bindings added to
+    it. This allows for tracing of the additions to the map. *)
+module type S1 = LinkedMap.S1
+
+(** Functor building an implementation of the non-empty linked HAMT structure
+    given a HAMT structure over ordered types. *)
+module Make1 (Hamt : HamtMisc.S) : S1 with type key = Hamt.key
