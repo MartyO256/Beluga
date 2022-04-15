1a180dbc1caf9aaaa65646d1a9343154ab91e0b1
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-04-15 01:59:18 -0400

    Implement non-empty linked map data structure

--- src/support/linkedMap.ml
+++ src/support/linkedMap.ml
@@ -30,6 +30,35 @@ module type S = sig
   val find_opt : key -> 'a t -> 'a option
 end
 
+module type S1 = sig
+  type key
+
+  type 'a t
+
+  val singleton : key -> 'a -> 'a t
+
+  val add : key -> 'a -> 'a t -> 'a t
+
+  val mem : key -> 'a t -> bool
+
+  val iter : (key -> 'a -> unit) -> 'a t -> unit
+
+  val iter_bindings : (key -> 'a -> unit) -> 'a t -> unit
+
+  val fold_left : (key -> 'b -> 'a) -> ('a -> key -> 'b -> 'a) -> 'b t -> 'a
+
+  val fold_right : (key -> 'a -> 'b) -> (key -> 'a -> 'b -> 'b) -> 'a t -> 'b
+
+  val fold_bindings :
+    (key -> 'a -> 'b) -> (key -> 'a -> 'b -> 'b) -> 'a t -> 'b
+
+  val exists : (key -> 'a -> bool) -> 'a t -> bool
+
+  val for_all : (key -> 'a -> bool) -> 'a t -> bool
+
+  val find_opt : key -> 'a t -> 'a option
+end
+
 module Make (Map : Map.S) : S with type key = Map.key = struct
   type key = Map.key
 
@@ -60,6 +89,50 @@ module Make (Map : Map.S) : S with type key = Map.key = struct
     List.fold_right (fun (key, value) acc -> f key value acc) list init
 
   let fold_bindings f { map; _ } init = Map.fold f map init
+
+  let exists p { map; _ } = Map.exists p map
+
+  let for_all p { map; _ } = Map.for_all p map
+
+  let find_opt key { map; _ } = Map.find_opt key map
+end
+
+module Make1 (Map : Map.S) : S1 with type key = Map.key = struct
+  type key = Map.key
+
+  type 'a t =
+    { list : (key * 'a) Nonempty.t
+    ; map : 'a Map.t
+    }
+
+  let mem key { map; _ } = Map.mem key map
+
+  let add key value { list; map } =
+    { list = Nonempty.cons (key, value) list; map = Map.add key value map }
+
+  let singleton key value =
+    { list = Nonempty.singleton (key, value); map = Map.singleton key value }
+
+  let iter f { list; _ } =
+    Nonempty.iter (fun (key, value) -> f key value) list
+
+  let iter_bindings f { map; _ } = Map.iter f map
+
+  let fold_left sing cons =
+    Nonempty.fold_left
+      (fun (key, value) -> sing key value)
+      (fun acc (key, value) -> cons acc key value)
+    |> fun go { list; _ } -> go list
+
+  let fold_right sing cons =
+    Nonempty.fold_right
+      (fun (key, value) -> sing key value)
+      (fun (key, value) acc -> cons key value acc)
+    |> fun go { list; _ } -> go list
+
+  let fold_bindings sing cons { map; list } =
+    let key, value = Nonempty.head list in
+    Map.fold cons (Map.remove key map) (sing key value)
 
   let exists p { map; _ } = Map.exists p map
 
--- src/support/linkedMap.mli
+++ src/support/linkedMap.mli
@@ -86,3 +86,86 @@ end
 (** Functor building an implementation of the linked map structure given a
     map structure over ordered types. *)
 module Make (Map : Map.S) : S with type key = Map.key
+
+(** Non-empty linked association tables over ordered types.
+
+    These are finite maps that keep track the sequence of bindings added to
+    it. This allows for tracing of the additions to the map. *)
+module type S1 = sig
+  (** The type of the map keys. *)
+  type key
+
+  (** The type of linked maps from type {!type:key} to type ['a]. *)
+  type 'a t
+
+  (** {1 Constructors} *)
+
+  (** [singleton key data] returns the one-element map that contains a
+      binding [data] for [key]. *)
+  val singleton : key -> 'a -> 'a t
+
+  (** [add key data m] returns a linked map containing the same bindings as
+      [m], plus a binding of [key] to [data]. If [key] was already bound in
+      [m] to a value, then the previous binding of [key] in [m] disappears,
+      except from the trace of additions. This addition is also recorded as
+      part of the trace of binding additions to the linked map. *)
+  val add : key -> 'a -> 'a t -> 'a t
+
+  (** {1 Predicates} *)
+
+  (** [mem x m] returns [true] if [m] contains a binding for [x], and [false]
+      otherwise. *)
+  val mem : key -> 'a t -> bool
+
+  (** {1 Iterators} *)
+
+  (** [iter f m] applies [f] to all the additions to [m] made using
+      {!val:add}. This includes bindings that were replaced in the map. [f]
+      receives the key as first argument, and the associated value as second
+      argument. *)
+  val iter : (key -> 'a -> unit) -> 'a t -> unit
+
+  (** [iter_bindings f m] applies [f] to all the current bindings in map [m].
+      [f] receives the key as first argument, and the associated value as
+      second argument. *)
+  val iter_bindings : (key -> 'a -> unit) -> 'a t -> unit
+
+  (** [fold_left f g m init] computes [g (... (g (f k1 d1) k2 d2) ...) km dm]
+      where [k1 ... km] are all the keys of all additions to [m], and
+      [d1 ... dm] are the associated data. Note that some of the traversed
+      bindings may not be part of the current bindings in the map. *)
+  val fold_left : (key -> 'b -> 'a) -> ('a -> key -> 'b -> 'a) -> 'b t -> 'a
+
+  (** [fold_right f g m init] computes
+      [g k1 d1 (g k2 d2 (... (f km dm) ...))] where [k1 ... km] are all the
+      keys of all additions to [m], and [d1 ... dm] are the associated data.
+      Note that some of the traversed bindings may not be part of the current
+      bindings in the map. *)
+  val fold_right : (key -> 'a -> 'b) -> (key -> 'a -> 'b -> 'b) -> 'a t -> 'b
+
+  (** [fold_bindings f g m init] computes [(g kN dN ... (f k1 d1)...)], where
+      [k1 ... kN] are the keys of all bindings in [m], and [d1 ... dN] are
+      the associated data. *)
+  val fold_bindings :
+    (key -> 'a -> 'b) -> (key -> 'a -> 'b -> 'b) -> 'a t -> 'b
+
+  (** {1 Scanning} *)
+
+  (** [exists p m] checks if at least one of the current bindings of [m]
+      satisfies the predicate [p]. [p] receives the key as first argument,
+      and the associated value as second argument. *)
+  val exists : (key -> 'a -> bool) -> 'a t -> bool
+
+  (** [for_all p m] checks if all of the current bindings of [m] satisfies
+      the predicate [p]. [p] receives the key as first argument, and the
+      associated value as second argument. *)
+  val for_all : (key -> 'a -> bool) -> 'a t -> bool
+
+  (** [find_opt key m] returns [Some v] if the current value of [key] in [m]
+      is [v], or [None] if no binding for [key] exists. *)
+  val find_opt : key -> 'a t -> 'a option
+end
+
+(** Functor building an implementation of the non-empty linked map structure
+    given a map structure over ordered types. *)
+module Make1 (Map : Map.S) : S1 with type key = Map.key
