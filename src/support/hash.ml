64830217851c65f4311b39be74441bba9388c671
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-05-02 12:00:47 -0400

    Implement `Hash.contramap`

--- src/support/hash.ml
+++ src/support/hash.ml
@@ -11,3 +11,14 @@ end) : HASH with type t = T.t = struct
 
   let hash = Hashtbl.hash
 end
+
+let contramap (type t t') (hash : (module HASH with type t = t'))
+    (f : t -> t') =
+  (module struct
+    type nonrec t = t
+
+    let hash x =
+      let (module Hash) = hash in
+      Hash.hash (f x)
+  end : HASH
+    with type t = t)
