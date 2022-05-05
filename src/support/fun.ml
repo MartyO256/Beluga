2fd5b943bba7642d5267df61f481f549b6c4e026
Marc-Antoine Ouimet <ouimetmarcantoine@gmail.com>
2022-05-04 21:48:56 -0400

    Add conventional function composition operator `( >> )`

--- src/support/fun.ml
+++ src/support/fun.ml
@@ -1,18 +1,22 @@
 include Stdlib.Fun
 
-let (++) f g x = f (g x)
+let ( ++ ) f g x = f (g x)
+
+let ( >> ) f g x = x |> f |> g
 
 let[@inline] apply x f = f x
 
 let flip f y x = f x y
 
-let rec until f =
-  if f () then until f
-  else ()
+let rec until f = if f () then until f else ()
 
-let[@inline] through f x = f x; x
+let[@inline] through f x =
+  f x;
+  x
 
-let after f x = f (); x
+let after f x =
+  f ();
+  x
 
 let[@inline] curry f x y = f (x, y)
 
